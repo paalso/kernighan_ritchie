dfbd510225690b1f155fd5125a60ad6d82a786dc
Pavel Soroka <40a.pvl@gmail.com>
2019-05-24 19:46:39 +0300

    Exersice 3-3 (expand)

--- 3/3-3.c
+++ 3/3-3.c
@@ -6,14 +6,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <ctype.h>
-#include <string.h>
 
-#define MAX_LEN 1000
-#define TAB_POS 8
+#define MAX_LEN 100
 
 void expand(char[], char[]);
-void append(char[], char[]);
-void generate_alphabetical_line(char, char, char[]);
+bool issamecase(char, char);
 
 // -------------------------------- Main --------------------------------
 int main(void) {
@@ -21,7 +18,8 @@ int main(void) {
     char s[MAX_LEN], s1[MAX_LEN];
 
     printf("Enter a string:\n");
-    scanf("%s",s);
+    // scanf("%s",s);
+    gets(s);
 
     expand(s,s1);
 
@@ -30,3 +28,38 @@ int main(void) {
     return 0;
 }
 
+// -----------------------------------------------------------------------
+void expand(char s[], char t[]) {
+    // for (int i = 1; (c = s[i]) != '\0'; ++i) {
+    char c;
+    int i = 1;
+    int j = 1;
+    t[0] = s[0];
+    while((c = s[i]) != '\0') {
+        if (c == '-')
+        {
+            ++i;
+            if (s[i] == '\0') {
+                t[j++] = s[i - 1];
+                break;
+            }
+
+            c = s[i];
+            if ((isalpha(c) || isdigit(c)) && issamecase(c, s[i - 2]) && c > s[i - 2])
+                for (int k = 1, len = c - s[i - 2]; k < len; ++k, ++j)
+                    t[j] = s[i - 2] + k;
+            else
+                t[j++] = s[i - 1];
+        } else
+            t[j++] = s[i++];
+
+    }
+    t[j] = '\0';
+}
+
+// -----------------------------------------------------------------------
+bool issamecase(char c1, char c2) {
+    if ((isupper(c1) && isupper(c2) || (islower(c1) && islower(c2)) || (isdigit(c1) && isdigit(c2))))
+        return true;
+    return false;
+}
---
+++ 3/shellsort.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int shellsort(int*, int);
+void swap(int*, int, int);
+void print_int_arr(int*, int);
+
+// -------------------------------- Main --------------------------------
+int main(void) {
+    int array[] = {5, 15, 0, 1, 12, 0, 7, 5, 6, 2, 4, 1};
+    int len = 12;
+
+    printf("Original array:\n");    
+    print_int_arr(array, len);
+
+    shellsort(array, len);
+    printf("\nSorted array:\n");    
+    print_int_arr(array, len);
+
+    return 0;
+}
+
+// ---------------------------------------------------------
+int shellsort(int array[], int len) {
+
+    for (int gap = len / 2; gap > 0; gap \= 2) {
+
+        for (int i = gap; i < len; ++i) {
+            
+        }
+        
+    }
+    
+    // swap(array, 0, 2);
+
+}
+
+
+// ---------------------------------------------------------
+void print_int_arr(int *array, int size) {
+    for (int *array_end = array + size; array < array_end; ++array)
+        printf("%d   ", *array);
+        printf("\n");
+} 
+
+// ---------------------------------------------------------
+void swap(int *array, int i, int j) {
+    int tmp = array[i];
+    array[i] = array[j];
+    array[j] = tmp;
+}
