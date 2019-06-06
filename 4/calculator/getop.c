3dbebf8d3a7d283323f62deeb7ac7b6faca74011
Pavel Soroka <40a.pvl@gmail.com>
2019-06-06 12:51:17 +0300

    Chapter 4. Project Calculator

---
+++ 4/calculator/getop.c
@@ -0,0 +1,30 @@
+#include <string.h>
+#include <stdio.h>
+
+#define NUMBER '0'
+
+
+int getch(void);
+void ungetch(int);
+
+/* getop: получает cледующий оператор или операнд */
+int getop(char s[])
+{
+    int i, c;
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+    if (!isdigit(c) && c != '.')
+        return c;    /* не чиcло */
+    i = 0;
+    if (isdigit(c)) /* накапливаем целую чаcть */
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    if (c =='.') /* накапливаем дробную чаcть */
+    while (isdigit(s[++i] = c = getch()))
+            ;
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+}
---
+++ 4/calculator/stack.c
@@ -0,0 +1,24 @@
+#define MAXVAL 100  /* максимальная глубина стека */
+
+int sp = 0;         /* следующая свободная позиция в стеке */
+double val[MAXVAL]; /* стек */
+
+/* push: положить значение f в стек */ 
+void push(double f)
+{
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("ошибка: стек полон, %g не помещается\n", f);
+}
+
+/* pop: взять с вершины стека и выдать в качестве результата */
+double pop(void)
+{
+    if (sp > 0)
+        return val[--sp];
+    else {
+        printf ("ошибка: стек пуст\n");
+    return 0.0;
+    }
+}
