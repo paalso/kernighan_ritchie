e139d256ae89a0850cca474caf1011af3b358c5a
Pavel Soroka <40a.pvl@gmail.com>
2019-07-12 21:20:21 +0300

    1-13: Horizontal Histogram, better version

--- 1/1-13_hor.c
+++ 1/1-13_hor.c
@@ -5,8 +5,9 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+#define SYMBOL '#'
 #define MAX_LENGTH 45
-#define HISTO_LEN_PER_ITEM 10
+#define MAX_HISTO_ROW_LEN 40
 
 void printHorizontalHistogram(int);
 
@@ -49,34 +50,46 @@ int main(void)
     int upper_lim = MAX_LENGTH;
     while (word_length[upper_lim] == 0)
         upper_lim -= 1;
+    
+    int max_index = 0;                      // соответствует наиболее часто встречающейся длине слова
+    for (int i = 1; i <= upper_lim; ++i)             // будет соответствать ряду гистограммы
+        if (word_length[i] > word_length[max_index]) // максимальной фиксированной длиной 
+                max_index = i;                       // MAX_HISTO_ROW_LEN
 
-    // Find non-zero elements number
-    int non_zero = 0;
-    for (int i = 0; i <= upper_lim; ++i)
-        if (word_length[i])
-            non_zero += 1;
+    // масштаб пересчета в длину горизонтального ряда гистограммы
+    float scale = (float) word_length[max_index] / MAX_HISTO_ROW_LEN;
 
-    // Version 1
-    // Способ печати диаграм не вполне удовлетворителен: в нек-х случаях самый длинный столбец
-    // может выходить за пределы экрана
+    int hysto_lens[upper_lim];              // массив с длинами рядов гистограмм
+    for (int i = 0; i <= upper_lim; ++i) 
+        hysto_lens[i] = (int)(word_length[i] / scale);
+
+    for (; hysto_lens[upper_lim] == 0; --upper_lim) // последний ряд с ненулевой длиной гистограммы
+        ;
 
     printf("Words length distribution info\n");
     printf("===============================\n\n");
     for (int i = 1; i <= upper_lim; ++i) {
         share = (float) word_length[i] / total_words;
         printf("%2d : %5.1f%%   ", i, share * 100);
-        printHorizontalHistogram((int)(share * HISTO_LEN_PER_ITEM * non_zero));
+        printHorizontalHistogram(hysto_lens[i]);
     }
-    share = (float) word_length[0] / total_words;
+
+    // вычисляем значение share и длину гистограммы в ряду "прочие"
+    int sum_of_others = word_length[0];
+    for (int i = upper_lim + 1; i < MAX_LENGTH; ++i)
+        sum_of_others += word_length[i];
+
+    share = (float) sum_of_others / total_words;
     printf(">  : %5.1f%%   ", share * 100);
-    printHorizontalHistogram((int)(share * HISTO_LEN_PER_ITEM * non_zero));
+    printHorizontalHistogram((int)(sum_of_others / scale));
 
     return 0;
 }
 
 void printHorizontalHistogram(int count) {
     for (int i = 0; i < count; ++i) {
-        putchar('*');
+        putchar(SYMBOL);
     }
     putchar('\n');
 }
+ 
